52c0392d9292a5c8cf2beffde1820adfeec6b48c
didikz <rezd14@gmail.com>
2015-05-07 10:12:17 +0700

    add home

---
+++ app/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('content')
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h4 class="panel-title">Home Dashboard</h4>
+  </div>
+  <div class="panel-body">
+    buat content / form di sini
+  </div>
+</div>  
+@stop
